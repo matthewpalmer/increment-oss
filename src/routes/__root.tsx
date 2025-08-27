e823a83d5d4f472124c4b277fab37aaafe67fa93
Matthew Palmer <matt@matthewpalmer.net>
2025-08-27 10:56:52 +0200

    Clean up about page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,6 @@
 import { createRootRouteWithContext, HeadContent, Link, Outlet } from '@tanstack/react-router';
 import { QueryClient } from '@tanstack/react-query';
-import { FakeSyncEngineControlPanel } from '../components/common/fake-sync-engine-control-panel';
 import { Theme } from "@radix-ui/themes";
-import { ShadowOuterIcon } from '@radix-ui/react-icons';
 import { SyncStatusDisplay } from '../components/sync/sync-status';
 export interface AppRouterContext {
     queryClient: QueryClient
@@ -17,13 +15,13 @@ export const Route = createRootRouteWithContext<AppRouterContext>()({
     component: () => (
         <>
             <HeadContent />
-            
+
             <div>
                 <Theme>
                     <div className="flex gap-2  p-4 justify-between align-bottom">
                         <Link to="/app" className="text-xs gap-1 text-indigo-500 hover:text-indigo-700 font-semibold flex items-center">
                             <img className='opacity' width="20" src="/increment-favicon.svg"></img>
-                             Increment
+                            Increment
                         </Link>
 
                         <SyncStatusDisplay />
